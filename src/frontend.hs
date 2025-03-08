ecd52bf554185437baccaebc5831e228ed4e6490
Raphaël Faure <raphael.faure2@student-cs.fr>
2025-03-08 20:50:38 +0100

    Improved syntax, C style block

--- src/frontend.hs
+++ src/frontend.hs
@@ -8,7 +8,6 @@ import Data.Char (ord)
 
 import Grammar ( Expr (Number, Boolean, Sum, Minus, If, Var, Let, Compare, Assign, Block))
 import Parser
-import Debug.Trace (trace)
 
 char :: Char -> Parser Char
 char c = Parser charP where
@@ -26,9 +25,6 @@ lock:: Char -> Parser Char
 lock c = Parser charLock where
     charLock [] = Nothing
     charLock (x:xs) | x == c = Nothing
-                    | x == '(' = Nothing
-                    | x == '\n' = Nothing
-                    | x == ';' = Nothing
                     | otherwise = Just (x, xs)
 
 until' :: Char -> Parser String
@@ -59,34 +55,52 @@ parseLitterals = (Number <$> int) <|>
 
 parseIf = do
     string "if" *> ss
+    char '('
+    ss
     cond <- parseExpr <* ss
-    char ':' <* ss
+    char ')'
+    ss
     then_instr <- parseBlock <* ss
-    string "else" <* ss  <* char ':' <* ss
+    string "else" <* ss <* ss
     else_instr <- parseBlock <*ss
     return $ If cond then_instr else_instr
 
 remaining :: Parser String
 remaining = Parser $ \s -> Just (s, s)
 
-nameChecker :: String -> Parser String
-nameChecker [] =  Parser $ const Nothing
-nameChecker (x:xs) | x == '(' = Parser $ const Nothing
-                   | x == ')' = Parser $ const Nothing
-                   | otherwise = return (x:xs)
+nameChecker :: String -> Maybe String
+nameChecker [] = Nothing
+nameChecker "let" = Just "Forbidden"
+nameChecker "if" = Just "Forbidden"
+nameChecker "else" = Just "Forbidden"
+nameChecker (x:xs)  | x == '(' = Just $ show x  ++ "not allowed in variable name"
+                    | x == ')' = Just $ show x  ++ "not allowed in variable name"
+                    | x == '{' = Just $ show x  ++ "not allowed in variable name"
+                    | x == '}' = Just $ show x  ++ "not allowed in variable name"
+                    | otherwise = nameChecker xs
 
+var = Parser $ \s ->
+    let v = runParser (until' ' ') s in
+    case v of
+        Just (o, s') -> case nameChecker o of
+            Nothing -> Just (o,s')
+            Just _ -> Nothing
+        Nothing -> Nothing
 
-varName = until' ' ' >>= nameChecker
+varName = var
+
 parseVar = Var <$> varName
 
 parseLet = do
     string "let" <* ss
-    id <- until' ' ' <* ss
+    id <- varName <* ss
     char '=' <* ss
-    value <- parseExpr
+    value <- parseExpr <* ss
+    char ';'
     return $ Let id value
 
-parseExpr = parseBinOp <|> parseAssign <|> parseLitterals <|> parseIf <|> parseLet <|> parseVar
+parseExpr = parseBinOp <|> parseAssign <|> parseLitterals <|> parseIf <|> parseLet  <|> parseVar <|> parseBlock
+
 
 parseExpr' =  parseLitterals <|> parseIf <|> parseLet <|> parseVar
 
@@ -114,12 +128,17 @@ parseAssign = do
     ss
     e <- parseExpr
     ss
+    char ';'
     return $ Assign v e
 
 parseBlock = do
-    ex <- some (parseExpr <* ss <* char ';' <* ss )
+    char '{'
+    ss
+    ex <- many (parseExpr <* ss )
+    ss
+    char '}'
     return $ Block ex
 
-parseAll s = case runParser parseBlock s of 
+parseAll s = case runParser parseBlock ("{" ++ s ++ "}") of 
     (Just (e, v)) -> e
     
